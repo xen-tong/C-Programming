b0beee094f9c36918ae28dde854773b614ced90f
Sabab Mustara Taranggo <xen.taranggo@gmail.com>
2025-11-04 10:42:16 +0600

    Add a Basic C-Program File...

---
+++ day01revise.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main(){
+printf("Enter Your Value Of a:");
+int a;
+scanf("%d",&a);
+printf("Enter Your Value Of b:");
+int b;
+scanf("%d",&b);
+printf("SUM of A,B:%d\n",a+b);
+printf("SUB of A,B:%d\n",a-b);
+printf("MUL of A,B:%d\n",a*b);
+printf("DIV of A,B:%d\n",a/b);
+
+printf("========================================\n");
+printf("========================================\n");
+
+
+//area of square
+printf("Enter Value OF C For CALCULATE Area Of Square:");
+float c;
+scanf("%f",&c);
+printf("Here The Solution Of  Area Of Square:%f \n",c*c);
+printf("========================================\n");
+printf("========================================\n");
+//area of circle
+printf("Enter Value OF C For CALCULATE Area Of Circle:");
+float d;
+scanf("%f",&d);
+printf("Here The Solution Of  Area Of Circle:%f \n",3.14*d*d);
+printf("========================================\n");
+printf("========================================\n");
+
+//area of rectangle
+
+printf("Enter Value OF e For CALCULATE Area Of Rectangle:");
+float e;
+scanf("%f",&e);
+printf("Enter Value OF f For CALCULATE Area Of Rectangle:");
+float f;
+scanf("%f",&f);
+printf("Here The Solution Of  Area Of Rectangle:%f\n ",e*f);
+printf("========================================\n");
+printf("=============END========================\n");
+printf("========================================\n");
+
+return 0;
+}
