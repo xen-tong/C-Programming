5a23322ef4c6f7488ac76377a44f057f11c6c676
Sabab Mustara Taranggo <xen.taranggo@gmail.com>
2025-11-04 15:23:29 +0600

    Basic
    Added a program to demonstrate relational operators in C.

--- relational_operators.c
+++ relational_operators.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+int main(){
+printf("Enter A:");
+int a;
+scanf("%d",&a);
+printf("Enter B:");
+int b;
+scanf("%d",&b);
+
+printf("For Equal to:%d\n",a==b);
+printf("For Greater-Than(if A Grater-than B):%d\n",a>b);
+printf("For Greater-Than Equal:%d\n",a>=b);
+printf("For Less-Than(if A Less-than B):%d\n",a<b);
+printf("For Less-Than Equal:%d\n",a<=b);
+printf("For Not Equal To:%d\n",a!=b);
+
+printf("===============Conditions===============\n");
+printf("If It Shows 1 Thats Mean It's True\n");
+printf("If It Shows 0 Thats Mean It's False");
+
+return 0;
+
+
+
+
+}
+
+
+
 
