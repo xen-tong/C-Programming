e0afd3c7b7c00a6c3ed09197c76eb67cba0fc437
Sabab Mustara Taranggo <xen.taranggo@gmail.com>
2025-12-05 07:05:20 +0600

    Loop Related

---
+++ 6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+
+int row;
+scanf("%d",&row);
+
+
+for(int i=row;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            printf("%d",j);
+        }
+
+    printf("\n");
+}
+
+
+
+
+}
+
