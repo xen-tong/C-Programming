79e3f554434c6405b6258fbb6f7d598ea79b62e8
Sabab Mustara Taranggo <xen.taranggo@gmail.com>
2026-01-03 05:57:26 +0600

    Add files via upload
    Pattern Wise

---
+++ Untitled4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+
+
+int n,row,col;
+scanf("%d",&n);
+for(row=1;row<=n;row++){
+    for(col=1;col<=row;col++){
+        if(row%2==0){
+            printf("0 ");
+        }
+        else{
+            printf("1 ");
+        }
+    }
+    printf("\n");
+}
+}
